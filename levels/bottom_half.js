55af14e56e22c9f8605048e82b9a3cdb3734e01b
Artem Bolshakov <a.t.bolsh@gmail.com>
2021-06-05 15:33:44 -0400

    Debugging cleaned up; a couple ideas sketched. Smooth motion next, funcs and user-friendly. Then, python env, then done.

---
+++ levels/bottom_half.js
@@ -0,0 +1,17 @@
+let reward = 0;
+
+let x = 200;
+let y = 600;
+let r = 40;
+
+let gold_r = 10;
+
+let gold = [];
+
+let wall = [
+[0, 0, 50, 800, 0], 
+[0, 0, 350, 50, 0],
+[450, 0, 350, 50, 0],
+[0, 800 - 50, 800, 50, 0], /* I know it's 750, doing this for clarity */
+[800 - 50, 0, 50, 800, 0]
+];
---
+++ levels/tool_use_advanced.js
@@ -0,0 +1,23 @@
+let reward = 0;
+
+/* Vastly incomplete; needs a tool, practice using it, and a way to get the gold with the tool. Later in program. */
+
+let x = 600;
+let y = 600;
+let r = 40;
+
+let gold_r = 10;
+
+let gold = [[170, 269]];
+
+let wall = [
+[0, 0, 50, 800, 0], 
+[0, 0, 800, 50, 0],
+[0, 800 - 50, 800, 50, 0],
+[800 - 50, 0, 50, 800, 0],
+
+[100, 100, 50, 450, 0], 
+[100, 100, 50, 450, - Math.PI/6]
+];
+
+
