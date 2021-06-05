509cd6fdcbed526e6ed984e048c510b0e9e2e60d
Artem Bolshakov <a.t.bolsh@gmail.com>
2021-06-05 08:18:01 -0400

    Split into 2 parts; diagonal wall next

--- engine.js
+++ engine.js
@@ -1,25 +1,9 @@
-/* Hello World example, stolen from the web editor.*/
 
 function setup() {
   createCanvas(800, 800);
   noLoop();
   noStroke();
 }
-
-let reward = 0;
-
-let x = 50;
-let y = 500;
-let r = 40;
-
-let gold_r = 10;
-
-let gold = [[200, 200], [220, 200], [250, 250], [450, 30]];
-
-
-/* first 2 are TOP CORNER coordinates, second 2 are width and height */
-
-let wall = [[650, 300, 50, 450], [300, 650, 400, 50]];
 
 function draw() {
   let agent_color = color(0, 255, 100);
---
+++ initial_sketch.js
@@ -0,0 +1,14 @@
+let reward = 0;
+
+let x = 50;
+let y = 500;
+let r = 40;
+
+let gold_r = 10;
+
+let gold = [[200, 200], [220, 200], [250, 250], [450, 30]];
+
+/* first 2 are TOP CORNER coordinates, second 2 are width and height */
+let wall = [[650, 300, 50, 450], [300, 650, 400, 50]];
+
+
